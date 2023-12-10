b610f16195bef61d6225c21481b8abf0a4af63cb
Simon Schödler <simon.schoedler@hotmail.ch>
2023-12-10 22:33:36 +0100

    Adds expected vs out comparison, adds expeted test results

--- NxVm.c
+++ NxVm.c
@@ -82,7 +82,79 @@ static void run_file(const wchar_t* path) {
 }
 
 // TODO: Move to test.c
-static InterpretResult run_test(const wchar_t* path) {
+const char** compare_strings_by_line(const char* a,
+                                     const char* b,
+                                     int* num_diff) {
+  // Temporary variables for processing
+  char* temp_a = strdup(a);
+  char* temp_b = strdup(b);
+  char* line_a;
+  char* line_b;
+  char* context_a = NULL;
+  char* context_b = NULL;
+
+  // Count the number of lines in a for allocating the array
+  int line_count_a = 0;
+  for (int i = 0; temp_a[i]; i++) {
+    if (temp_a[i] == '\n') {
+      line_count_a++;
+    }
+  }
+
+  const char** diff = malloc(sizeof(char*) * (line_count_a + 1));
+  if (diff == NULL) {
+    INTERNAL_ERROR("Not enough memory to allocate diff array");
+    exit(74);
+  }
+
+  // Start tokenizing and comparing
+  int diff_count = 0;
+  line_a = strtok_s(temp_a, "\r\n", &context_a);
+  line_b = strtok_s(temp_b, "\r\n", &context_b);
+  int line_no = 0;
+
+  // TODO: Add warning if b has more lines than a
+  // TODO: Add warning a has no lines
+
+  while (line_a != NULL) {
+    line_no++;
+    // If line_b is NULL, it means b has fewer lines than a
+    if (line_b == NULL) {
+      break;
+    }
+
+    if (strcmp(line_a, line_b) != 0) {
+      // Lines are different, format the string and add to diff
+      char* diff_line = malloc(
+          1024);  // Allocating a fixed size, should be enough for most lines
+      if (diff_line == NULL) {
+        INTERNAL_ERROR("Not enough memory to allocate diff line");
+        exit(74);
+      }
+      sprintf(diff_line,
+              "on line %d " ANSI_GREEN_STR("%s") " was " ANSI_RED_STR("%s"),
+              line_no, line_a, line_b);
+      diff[diff_count++] = diff_line;
+    }
+
+    // Move to the next line
+    line_a = strtok_s(NULL, "\r\n", &context_a);
+    line_b = strtok_s(NULL, "\r\n", &context_b);
+  }
+
+  // Finalize the array
+  diff[diff_count] = NULL;
+  *num_diff = diff_count;
+
+  // Clean up
+  free(temp_a);
+  free(temp_b);
+
+  return diff;
+}
+
+// TODO: Move to test.c
+static bool run_test(const wchar_t* path) {
   init_vm();
 
   char* source = read_file(path);
@@ -105,14 +177,62 @@ static InterpretResult run_test(const wchar_t* path) {
   InterpretResult result = interpret(source);
 
   free(source);
-
   free_vm();
 
   // Restore stdout
   fclose(stream);
   freopen("CONOUT$", "w", stdout);
 
-  return result;
+  // Compare output with expect
+  wchar_t expect_filepath[MAX_PATH];
+  _stprintf(expect_filepath, _T("%s%s"), path, L".expect");
+  wchar_t* out_filepath[MAX_PATH];
+  _stprintf(out_filepath, _T("%s%s"), path, L".out");
+
+  char* expect = read_file(expect_filepath);
+  char* out = read_file(out_filepath);
+
+  bool passed = true;
+  int num_diff = 0;
+
+  const char** differences = compare_strings_by_line(expect, out, &num_diff);
+  if (num_diff > 0) {
+    passed = false;
+    printf(ANSI_RED_STR("Failed: %d lines differ!\n"), num_diff);
+    for (int i = 0; i < num_diff; i++) {
+      printf("        #%d: %s", i + 1, differences[i]);
+      wprintf(L" (in %s)\n", out_filepath);
+      free((void*)differences[i]);
+    }
+  }
+
+  free(expect);
+  free(out);
+  free(differences);
+
+  if (!passed) {
+    return passed;
+  }
+
+  switch (result) {
+    case INTERPRET_OK:
+      printf(ANSI_GREEN_STR("Passed!\n"));
+      break;
+    case INTERPRET_COMPILE_ERROR:
+      passed = false;
+      printf(ANSI_RED_STR("Failed: Compile Error!\n"));
+      break;
+    case INTERPRET_RUNTIME_ERROR:
+      passed = false;
+      printf(ANSI_RED_STR("Failed: Runtime Error!\n"));
+      break;
+    default:
+      printf(ANSI_RED_STR("Failed: Internal Error!\n"));
+      INTERNAL_ERROR("Unhandled InterpretResult");
+      break;
+  }
+
+  return passed;
 }
 
 // TODO: Move to test.c
@@ -125,7 +245,6 @@ static void run_tests(const wchar_t* path) {
     test_file_paths[i] = (wchar_t*)malloc(MAX_PATH * sizeof(wchar_t));
   }
 
-  // Scan the directory
   scan_tests_dir(path, test_file_paths, &count, max_files);
 
   if (count == 0) {
@@ -145,27 +264,10 @@ static void run_tests(const wchar_t* path) {
   int failed = 0;
   for (int i = 0; i < count; i++) {
     wprintf(L"    - running %s ", test_file_paths[i]);
-
-    InterpretResult result = run_test(test_file_paths[i]);
-
-    switch (result) {
-      case INTERPRET_OK:
-        printf(ANSI_GREEN_STR("Ok\n"));
-        break;
-      case INTERPRET_COMPILE_ERROR:
-        failed++;
-        printf(ANSI_RED_STR("Compile Error\n"));
-        break;
-      case INTERPRET_RUNTIME_ERROR:
-        failed++;
-        printf(ANSI_RED_STR("Runtime Error\n"));
-        break;
-      default:
-        printf(ANSI_RED_STR("INTERNAL ERROR\n"));
-        INTERNAL_ERROR("Unhandled InterpretResult");
-        break;
+    bool result = run_test(test_file_paths[i]);
+    if (!result) {
+      failed++;
     }
-
     free(test_file_paths[i]);
   }
 
