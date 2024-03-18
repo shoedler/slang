b2c83ff7a07f99d649ea0aa120c714e4dc299e02
Simon Schödler <simon.schoedler@hotmail.ch>
2024-03-18 01:23:58 +0100

    Updates launch configs to provide args to run the sample. Definetively removes the args override in main

--- main.c
+++ main.c
@@ -27,10 +27,6 @@ void usage() {
 }
 
 int main(int argc, char* argv[]) {
-  // init_vm();
-  // run_file("C:\\Projects\\slang\\sample.sl", "main");
-  // free_vm();
-
   // Print args
   if (argc == 2 && strcmp(argv[1], "repl") == 0) {
     init_vm();
