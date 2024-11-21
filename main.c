4218368888f25502a1347daab4a36edf73669b9a
Simon Schödler <simon.schoedler@hotmail.ch>
2024-11-21 21:07:03 +0100

    Adds a crude cli options handler, adds ability to toggle gc-stressing via cli

--- main.c
+++ main.c
@@ -5,7 +5,74 @@
 #include "common.h"
 #include "vm.h"
 
-static void repl() {
+#define CMD_REPL "repl"
+#define CMD_RUN "run"
+#define CMD_VERSION "--version"
+typedef struct {
+  char** argv;
+  int argc;
+} Options;
+
+static Options opts;
+
+static void init_options(int argc, char* argv[]) {
+  opts.argv = argv + 1;  // Skip program name
+  opts.argc = argc - 1;
+}
+
+static bool consume_option(const char* name) {
+  for (int i = 0; i < opts.argc; i++) {
+    if (strcmp(opts.argv[i], name) == 0) {
+      memmove(&opts.argv[i], &opts.argv[i + 1], (opts.argc - i - 1) * sizeof(char*));  // Shift the rest of the args left
+      opts.argc--;
+      return true;
+    }
+  }
+  return false;
+}
+
+static char* pop_option(void) {
+  if (opts.argc == 0) {
+    return NULL;
+  }
+  return opts.argv[--opts.argc];
+}
+
+static bool validate_options(void) {
+  return opts.argc == 0;
+}
+
+static void usage(const char* error) {
+  if (error != NULL) {
+    fprintf(stderr, "Error: %s\n", error);
+    printf("\n");
+  }
+  printf("Usage: slang <args>\n");
+  printf("  " CMD_RUN "  <options> <path> Run script at <path>\n");
+  printf("  " CMD_REPL " <options>        Run REPL\n");
+  printf("  " CMD_VERSION "             Print version\n");
+  printf("\n");
+  printf("  <options>:\n");
+  printf("    --gc-stress         Enable GC stress testing\n");
+}
+
+static void configure_vm() {
+  bool gc_stress = consume_option("--gc-stress");
+  if (gc_stress) {
+    INTERNAL_WARN("GC stress testing enabled, can be disabled during runtime using the Gc module.");
+    VM_SET_FLAG(VM_FLAG_PAUSE_GC);
+  }
+}
+
+static SlangExitCode repl() {
+  init_vm();
+  configure_vm();
+
+  if (!validate_options()) {
+    usage("Unknown options for " CMD_REPL);
+    exit(SLANG_EXIT_BAD_USAGE);
+  }
+
   // TODO (misc): Use linenoise for REPLing
   char line[1024];
 
@@ -17,7 +84,7 @@ static void repl() {
   }
 
   // Create a module initially to act as our toplevel module.
-  start_module(cwd, "repl");
+  start_module(cwd, CMD_REPL);
 
   for (;;) {
     printf("slang > ");
@@ -29,31 +96,56 @@ static void repl() {
 
     interpret(line, NULL, NULL);
   }
+
+  bool has_error = VM_HAS_FLAG(VM_FLAG_HAS_ERROR);
+  free_vm();
+
+  return has_error ? SLANG_EXIT_FAILURE : SLANG_EXIT_SUCCESS;
 }
 
-void usage() {
-  printf("Usage: slang <args>\n");
-  printf("  run  <path> Run script at <path>\n");
-  printf("  repl        Run REPL\n");
-  printf("  --version   Print version\n");
-  exit(SLANG_EXIT_BAD_USAGE);
+static SlangExitCode run() {
+  init_vm();
+  configure_vm();
+
+  const char* path = pop_option();
+  if (path == NULL) {
+    usage("No path provided for " CMD_RUN);
+    exit(SLANG_EXIT_BAD_USAGE);
+  }
+  if (!validate_options()) {
+    usage("Unknown options for " CMD_RUN);
+    exit(SLANG_EXIT_BAD_USAGE);
+  }
+
+  run_file(path, "main");
+
+  bool has_error = VM_HAS_FLAG(VM_FLAG_HAS_ERROR);
+  free_vm();
+
+  return has_error ? SLANG_EXIT_FAILURE : SLANG_EXIT_SUCCESS;
 }
 
 int main(int argc, char* argv[]) {
-  // Print args
-  if (argc == 2 && strcmp(argv[1], "repl") == 0) {
-    init_vm();
-    repl();
-    free_vm();
-  } else if (argc == 3 && strcmp(argv[1], "run") == 0) {
-    init_vm();
-    run_file(argv[2], "main");
-    free_vm();
-  } else if (argc == 2 && strcmp(argv[1], "--version") == 0) {
-    printf("slang %s\n", SLANG_VERSION);
-  } else {
-    usage();
+  if (argc < 2) {
+    usage("No arguments provided");
+    exit(SLANG_EXIT_BAD_USAGE);
   }
 
-  exit(SLANG_EXIT_SUCCESS);
+  init_options(argc, argv);
+
+  SlangExitCode code;
+
+  if (consume_option(CMD_REPL)) {
+    code = repl();
+  } else if (consume_option(CMD_RUN)) {
+    code = run();
+  } else if (consume_option("--version")) {
+    printf("slang %s\n", SLANG_VERSION);
+    code = SLANG_EXIT_SUCCESS;
+  } else {
+    usage("Invalid command");
+    code = SLANG_EXIT_BAD_USAGE;
+  }
+
+  exit(code);
 }
